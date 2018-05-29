637586191a4d3e996634805a1b5db33e390cd60d
Jason C <jforeza@gmail.com>
2018-05-29 11:02:58 -0700

    Add R4D6 DND roll functionality and some metric functions
    Add metrics_analyzeRoll to just get total and display. Metric functions can also be used to push to some web api for reporting at some point
    Update all utility function naming to be more consistent with rest of the functions in the app
    Resize the UI to better fit on small phones

--- OCGEN/StatGenController.swift
+++ OCGEN/StatGenController.swift
@@ -19,6 +19,7 @@ class StatGenController: UIViewController, ASAdViewDelegate {
     var isReady = false
     
      // Declare my view variables here
+    @IBOutlet weak var statSummary: UILabel!
     @IBOutlet weak var strValue: UILabel!
     @IBOutlet weak var dexValue: UILabel!
     @IBOutlet weak var conValue: UILabel!
@@ -61,24 +62,82 @@ class StatGenController: UIViewController, ASAdViewDelegate {
     
     @IBAction func doStatRoll(_ sender: Any) {
         
-        print("[DEBUG] Doing Stat Roll")
+        print("[DEBUG] doStatRoll - Doing Stat Roll")
+        
+        // Declare any sort of metric collection totals
+        var rollTotal = 0;
         
         for values in rollValues {
-            utilUpdateTextLabel(label: values, value: utilGetRandomNumber(value: 0))
+            
+            // Perform the roll
+            let rollVal = roll4D6DropLowest()
+            
+            // Do any sort of metrics collection here
+            rollTotal += rollVal
+            
+            // Update the view
+            util_updateTextLabel(label: values, value: rollVal)
         }
+        
+        // Perform metrics
+        metrics_analyzeRoll(type: 0, value: rollTotal)
 
+
+    }
+    
+    // Utility to calculate the4D6 roll
+    func roll4D6DropLowest() -> Int {
+        
+        var statArr = [0,0,0,0]        // Declare an array
+        
+        for (index, stat) in statArr.enumerated() {
+            let statSeed = util_getRandomNumber(value: 6)
+            statArr[index] = statSeed
+            print("[DEBUG] - utilRoll4D6DropLowest \(index) : \(statSeed)" )
+            
+        }
+        
+        let statVal = statArr.reduce(0, { x, y in
+            x + y
+        })
+        
+        print("[DEBUG] - roll4D6DropLowest statVal is : \(statVal)")
+        
+        
+        let finalStatVal = statVal - statArr.min()!
+        
+        print("[DEBUG] - roll4D6DropLowest finalStatVal is : \(finalStatVal) after dropping \(statArr.min()!)")
+        
+        
+        return finalStatVal
+    }
+    
+    // Function to analyze the roll that was just performed
+    func metrics_analyzeRoll(type: Int, value : Int){
+        
+        /*
+         roll4D6DropLowest = 0
+         roll3D6 = 1
+         roll 3D6 x3 highest = 2
+         */
+        
+        print ("[DEBUG] - metrics_analyzeRoll roll total was: \(value) ")
+        
+        statSummary?.text = "metrics_analyzeRoll roll total was: \(value)"
+        
     }
     
     // Utility to update a given text label
-    func utilUpdateTextLabel(label : UILabel!, value : Int){
-        
+    func util_updateTextLabel(label : UILabel!, value : Int){
         label?.text = String(value)
-
     }
     
-    // Utility to return a random number
-    func utilGetRandomNumber(value : Int) -> Int {
-        return 5
+    // Utility to return a random number using arc4random_uniform
+    func util_getRandomNumber(value : Int) -> Int {
+        let n = Int(arc4random_uniform(UInt32(value)))
+        return n + 1
     }
+    
+   
     
 }
