0f49d825fe6d71839e92bc81549f1651ac5370ba
Jason C <jforeza@gmail.com>
2018-05-29 10:19:38 -0700

    Add placeholder functions, buttons, and labels for StatRoller
    Modify StatGenController to include all the labels / @IABfunctions needed for a basic roll
    Updated the UI of the StatGenController as well as the other controllers so the top logo / display is more consistent

--- OCGEN/StatGenController.swift
+++ OCGEN/StatGenController.swift
@@ -12,8 +12,6 @@ import AerServSDK
 
 class StatGenController: UIViewController, ASAdViewDelegate {
     
-
-    
     // Get a copy of the VC instance
     var vc = VCInstance.sharedInstance
     
@@ -21,16 +19,26 @@ class StatGenController: UIViewController, ASAdViewDelegate {
     var isReady = false
     
      // Declare my view variables here
+    @IBOutlet weak var strValue: UILabel!
+    @IBOutlet weak var dexValue: UILabel!
+    @IBOutlet weak var conValue: UILabel!
+    @IBOutlet weak var intValue: UILabel!
+    @IBOutlet weak var wisValue: UILabel!
+    @IBOutlet weak var chrValue: UILabel!
     
-        // MARK: - VIEW - View controller functions
+    var rollValues = [UILabel]()
+    
+    // MARK: - VIEW - View controller functions
     
     // On the initial view load, do the following:
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Set the UI text elements for this view
+        
+        // Use rollValues to easily access the @IBOutlet values
+        rollValues = [strValue, dexValue,conValue, intValue, wisValue, chrValue]
 
-        // Load the banner
         
     }
     
@@ -49,6 +57,28 @@ class StatGenController: UIViewController, ASAdViewDelegate {
     
     func viewControllerForPresentingModalView() -> UIViewController! {
         return self
+    }
+    
+    @IBAction func doStatRoll(_ sender: Any) {
+        
+        print("[DEBUG] Doing Stat Roll")
+        
+        for values in rollValues {
+            utilUpdateTextLabel(label: values, value: utilGetRandomNumber(value: 0))
+        }
+
+    }
+    
+    // Utility to update a given text label
+    func utilUpdateTextLabel(label : UILabel!, value : Int){
+        
+        label?.text = String(value)
+
+    }
+    
+    // Utility to return a random number
+    func utilGetRandomNumber(value : Int) -> Int {
+        return 5
     }
     
 }
